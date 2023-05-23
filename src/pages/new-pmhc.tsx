c249cd8c38dbd16c1287a64bbe8a76614066ab96
Juliano Farias <thefrontendwizard@gmail.com>
2023-05-23 23:59:43 +0200

    refactor: extract fields away from jsx

--- src/pages/new-pmhc.tsx
+++ src/pages/new-pmhc.tsx
@@ -63,6 +63,93 @@ function TextInput({
   )
 }
 
+const fields: Pick<
+  TextInputProps,
+  'fieldKey' | 'label' | 'rules' | 'helperText'
+>[] = [
+  {
+    fieldKey: 'sequence',
+    label: 'Sequence',
+    rules: { required: 'Sequence is required' },
+    helperText: 'e.g. YRLACNCVED',
+  },
+  {
+    fieldKey: 'epitope_id_by_iedb',
+    label: 'Epitope id by iedb',
+    rules: { required: 'Epitope id is required' },
+  },
+  {
+    fieldKey: 'link_epitope_id_by_iedb',
+    label: 'IEDB link',
+    rules: { required: 'Link to epitope id on iedb is required' },
+  },
+  {
+    fieldKey: 'epitope_position',
+    label: 'Epitope Position',
+    rules: { required: 'Epitope position is required' },
+  },
+  {
+    fieldKey: 'structure_type_id',
+    label: 'Structure Type',
+    rules: { required: 'Structure type is required' },
+  },
+  {
+    fieldKey: 'link_para_structure_type',
+    label: 'Link to Structure Type',
+    rules: { required: 'Link to structure type is required' },
+  },
+  {
+    fieldKey: 'structure_source',
+    label: 'Source Structure',
+    rules: { required: 'Source structure is required' },
+  },
+  {
+    fieldKey: 'mhc_allele_id',
+    label: 'MHC Allele id',
+    rules: { required: 'MHC Allele is required' },
+  },
+  {
+    fieldKey: 'source_organism',
+    label: 'Source Organism',
+    rules: { required: 'Source organism is required' },
+  },
+  {
+    fieldKey: 'immunological_background_id',
+    label: 'Immunological Background',
+    rules: { required: 'Immunological background is required' },
+  },
+  {
+    fieldKey: 'source_protein',
+    label: 'Source Protein',
+    rules: { required: 'Source Protein is required' },
+  },
+  {
+    fieldKey: 'link_para_source_protein',
+    label: 'Link to Source Protein',
+    rules: { required: 'Link to Source Protein is required' },
+  },
+  {
+    fieldKey: 'reference',
+    label: 'Reference',
+    rules: { required: 'Reference is required' },
+  },
+  {
+    fieldKey: 'link_para_reference',
+    label: 'Reference Link',
+    rules: { required: 'Reference Link is required' },
+  },
+  {
+    fieldKey: 'link_para_imagem',
+    label: 'Image Link',
+    rules: { required: 'Image Link is required' },
+  },
+  {
+    fieldKey: 'link_para_pdb_file',
+    label: 'PDB file Link',
+    rules: { required: 'PDB File Link is required' },
+  },
+]
+
 const NewPmhcPage: NextPage = () => {
   const utils = trpc.useContext()
   const { register, handleSubmit, formState } = useForm<FormValues>({
@@ -78,17 +165,7 @@ const NewPmhcPage: NextPage = () => {
   const onSubmit = handleSubmit((data) => {
     addPMHC.mutateAsync(data)
   })
-  const fields: Pick<
-    TextInputProps,
-    'fieldKey' | 'label' | 'rules' | 'helperText'
-  >[] = [
-    {
-      fieldKey: 'sequence',
-      label: 'Sequence',
-      rules: { required: 'Sequence is required' },
-      helperText: 'e.g. YRLACNCVED',
-    },
-  ]
+
   return (
     <Container maxW="container.lg" as="main">
       <Link href="/">
@@ -107,111 +184,6 @@ const NewPmhcPage: NextPage = () => {
                   {...field}
                 />
               ))}
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="epitope_id_by_iedb"
-                label="Epitope id by iedb"
-                rules={{ required: 'Epitope id is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_epitope_id_by_iedb"
-                label="IEDB link"
-                rules={{ required: 'Link to epitope id on iedb is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="epitope_position"
-                label="Epitope Position"
-                rules={{ required: 'Epitope position is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="structure_type_id"
-                label="Structure Type"
-                rules={{ required: 'Structure type is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_para_structure_type"
-                label="Link to Structure Type"
-                rules={{ required: 'Link to structure type is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="structure_source"
-                label="Source Structure"
-                rules={{ required: 'Source structure is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="mhc_allele_id"
-                label="MHC Allele id"
-                rules={{ required: 'MHC Allele is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="source_organism"
-                label="Source Organism"
-                rules={{ required: 'Source organism is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="immunological_background_id"
-                label="Immunological Background"
-                rules={{ required: 'Immunological background is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="source_protein"
-                label="Source Protein"
-                rules={{ required: 'Source Protein is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_para_source_protein"
-                label="Link to Source Protein"
-                rules={{ required: 'Link to Source Protein is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="reference"
-                label="Reference"
-                rules={{ required: 'Reference is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_para_reference"
-                label="Reference Link"
-                rules={{ required: 'Reference Link is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_para_imagem"
-                label="Image Link"
-                rules={{ required: 'Image Link is required' }}
-              />
-              <TextInput
-                register={register}
-                formState={formState}
-                fieldKey="link_para_pdb_file"
-                label="PDB file Link"
-                rules={{ required: 'PDB File Link is required' }}
-              />
             </SimpleGrid>
             <Button
               variant="solid"
